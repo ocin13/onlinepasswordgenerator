ad512f4616c12c6be5957baa56be543153899f0f
Hocine <hoc1ne13dec@gmail.com>
2020-09-24 17:09:38 -0400

    second commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,8 @@
 //slider button
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
+var value = document.getElementById("selectValue");
+value.innerHTML = slider.value;
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  value.innerHTML = this.value;
 }
